98e29f3e8ff0efabc5660e47a8a94eab53cb11be
Jurgen (J.S.) Hildebrand <jurgen.hildebrand@ing.com>
2019-04-12 10:34:42 +0200

    test mlcp

---
+++ nl-load-sample.bat
@@ -0,0 +1,5 @@
+ mlcp import -host localhost -port 8000 -username admin ^
+ -password "xxx" -input_file_path simple.zip -mode local -input_compressed true ^
+ -output_uri_replace "/C:/Users/cs79en.AD/Projects/tfs/target/simple.zip/simple/,''" ^
+ -streaming -output_uri_prefix /simple/ -input_file_type documents 
+ 
